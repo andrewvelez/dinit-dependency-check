1bbccda0b9298079cf635cd64a242a5c5c920edd
Andrew Velez <andrewvelez@users.noreply.github.com>
2026-04-06 03:21:55 -0400

    new build file

---
+++ buildandrun.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env bun
+/**
+ * build file for this project
+ */
+
+await Bun.build({
+  entrypoints: ["./ddepcheck.ts"],
+  compile: {
+    outfile: "./bin/dinit-dependency-graph",
+  },
+  minify: true,
+  sourcemap: "linked",
+  bytecode: true,
+});
+
+await $`./bin/dinit-dependency-graph`;
